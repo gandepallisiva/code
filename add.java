030fcb928d8ecae237bdb869a4689f39554212b6
gandepalli siva <siva0402143>
2022-06-23 16:37:40 +0530

    initial commit

---
+++ add.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class add {
+
+	public static void main(String[] args) {
+	int a,b,c;
+	System.out.println("ENTER THE TWO NUMBER");
+	Scanner s= new Scanner(System.in);
+	a=s.nextInt();
+	b=s.nextInt();
+	
+	
+	c=a=b;
+	System.out.println("addition="+c);
+	
+	
+
+
+	}
+
+}
