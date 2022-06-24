2787fc529f85fc38328f9e24c6b3924005463d4e
gandepalli siva <siva0402143>
2022-06-24 12:43:47 +0530

    initial commit

---
+++ secondprogram.java
@@ -0,0 +1,18 @@
+
+public class secondprogram {
+
+	public static void main(String[] args) {
+		int a=20;
+		float b=30;//implicent conversion
+		float d=33.9f;
+		int c=(int)d;//explicit conversion
+		
+
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(d);
+		System.out.println(c);
+		
+	}
+
+}
